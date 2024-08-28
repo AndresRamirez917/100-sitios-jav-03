daaecfb6806caa44ec9b1065498aaefa1cf40ef5
AndresRamirez917 <andresmintic2021@gmail.com>
2024-08-28 15:11:26 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const row = document.querySelector('.card ')
+const row = document.querySelector('.col-md-6 ')
+const about = document.querySelector('.about')
 
 async function getData() {
     const result = await fetch('https://jsonplaceholder.typicode.com/photos/')
@@ -7,10 +8,22 @@ async function getData() {
         if(element.id <= 1){
             const roww = document.createRange().createContextualFragment(`
 
- <img src="${element.url}" alt="" class="card-img-top img-fluid" />
+            <div class="img-container">
+                 <img src="${element.url}"  alt=""> 
+            </div>
 
                 `)
                 row.append(roww)
+            } else if (element.id == 2) {
+                const card = document.createRange().createContextualFragment(`
+
+                   <div class="col-md-6">
+                <img src="${element.url}" alt="">
+            </div>
+        
+                        `)
+                        about.append(card)
+                
             }
         });
     }
